f429ed6a5bb71623d2c15ca3e50e148cce630f6d
Ben Moss <bmoss40@bloomberg.net>
2025-10-30 09:55:15 -0400

    Disable LTO=fat

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 
 export OPENSSL_DIR=$PREFIX
 export CARGO_PROFILE_RELEASE_STRIP=symbols
-export CARGO_PROFILE_RELEASE_LTO=fat
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 cargo install --no-track --locked --root "$PREFIX" --path .
